de628c0b2d6133b167afb4b0e6517203cb91a419
Thomas Teague <thomas.d.teague1@gmail.com>
2021-11-06 08:23:47 -0400

    Added skeleton files for the Module 4 assignment

---
+++ src/main/java/com/project/module04/Traversals.java
@@ -0,0 +1,67 @@
+package com.project.module04;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Your implementation of the pre-order, in-order, and post-order
+ * traversals of a tree.
+ */
+public class Traversals<T extends Comparable<? super T>> {
+
+    /**
+     * DO NOT ADD ANY GLOBAL VARIABLES!
+     */
+
+    /**
+     * Given the root of a binary search tree, generate a
+     * pre-order traversal of the tree. The original tree
+     * should not be modified in any way.
+     *
+     * This must be done recursively.
+     *
+     * Must be O(n).
+     *
+     * @param <T> Generic type.
+     * @param root The root of a BST.
+     * @return List containing the pre-order traversal of the tree.
+     */
+    public List<T> preorder(TreeNode<T> root) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Given the root of a binary search tree, generate an
+     * in-order traversal of the tree. The original tree
+     * should not be modified in any way.
+     *
+     * This must be done recursively.
+     *
+     * Must be O(n).
+     *
+     * @param <T> Generic type.
+     * @param root The root of a BST.
+     * @return List containing the in-order traversal of the tree.
+     */
+    public List<T> inorder(TreeNode<T> root) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Given the root of a binary search tree, generate a
+     * post-order traversal of the tree. The original tree
+     * should not be modified in any way.
+     *
+     * This must be done recursively.
+     *
+     * Must be O(n).
+     *
+     * @param <T> Generic type.
+     * @param root The root of a BST.
+     * @return List containing the post-order traversal of the tree.
+     */
+    public List<T> postorder(TreeNode<T> root) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+}
---
+++ src/test/java/com/project/module04/TraversalsTests.java
@@ -0,0 +1,8 @@
+package com.project.module04;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TraversalsTests {
+
+}
