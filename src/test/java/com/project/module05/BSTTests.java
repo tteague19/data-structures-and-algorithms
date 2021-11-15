363e44a37fdcf027ff34f5f683b47db52dda01e8
Thomas Teague <thomas.d.teague1@gmail.com>
2021-11-15 17:07:09 -0500

    Removed unused imports

--- src/test/java/com/project/module05/BSTTests.java
+++ src/test/java/com/project/module05/BSTTests.java
@@ -2,8 +2,6 @@ package com.project.module05;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.junit.Test;
