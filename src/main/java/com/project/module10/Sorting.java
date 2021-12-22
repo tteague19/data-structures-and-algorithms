22c0f5d6a81773c31c0f515071164c440ac04936
Thomas Teague <thomas.d.teague1@gmail.com>
2021-12-22 07:59:59 -0600

    Created initial files for Module 10 assignment

---
+++ src/main/java/com/project/module10/Sorting.java
@@ -0,0 +1,77 @@
+package com.project.module10;
+
+import java.util.Comparator;
+
+/**
+ * Your implementation of various iterative sorting algorithms.
+ */
+public class Sorting {
+
+    /**
+     * Implement bubble sort.
+     *
+     * It should be:
+     * in-place
+     * stable
+     * adaptive
+     *
+     * Have a worst case running time of: O(n^2)
+     * And a best case running time of: O(n)
+     *
+     * NOTE: You should implement bubble sort with the last swap optimization.
+     *
+     * You may assume that the passed in array and comparator
+     * are both valid and will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Implement selection sort.
+     *
+     * It should be:
+     * in-place
+     * unstable
+     * not adaptive
+     *
+     * Have a worst case running time of: O(n^2)
+     * And a best case running time of: O(n^2)
+     *
+     * You may assume that the passed in array and comparator
+     * are both valid and will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void selectionSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Implement insertion sort.
+     *
+     * It should be:
+     * in-place
+     * stable
+     * adaptive
+     *
+     * Have a worst case running time of: O(n^2)
+     * And a best case running time of: O(n)
+     *
+     * You may assume that the passed in array and comparator
+     * are both valid and will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void insertionSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+}
---
+++ src/test/java/com/project/module10/SortingTests.java
@@ -0,0 +1,10 @@
+package com.project.module10;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class SortingTests {
+    
+}
